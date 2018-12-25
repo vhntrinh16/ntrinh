7c3dd56ce8df4135a845a358cdc3c941317e7c7d
ntrinh <trinhvu16k11@viendong.edu.vn>
2018-12-25 21:55:55 +0700

    HopSua

---
+++ bai608/bai608/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: dell
+ * Date: 11/29/2018
+ * Time: 10:25 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace bai608
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			clsHopSua a,b,c;
+			a = new clsHopSua();
+			a.DatNhanHieu("Vinamilk");
+			Console.WriteLine("Hop sua: {0}", a.LayNhanHieu());
+			//a.Nhap();
+			
+			b = new clsHopSua();
+			b.DatTrongLuong(10);
+			Console.WriteLine("Trong luong: {0}", b.XuatTL());
+			
+			c = new clsHopSua();
+			c.DatNgay(DateTime.Now);
+			Console.WriteLine("Han su dung: {0}", c.XuatNgay());
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+	}
+}
+		}
---
+++ bai608/bai608/clsHopSua.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: dell
+ * Date: 11/29/2018
+ * Time: 10:25 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace bai608
+{
+	/// <summary>
+	/// Description of clsHopSua.
+	/// </summary>
+	public class clsHopSua
+	{
+		public string NhanHieu;
+		public double TrongLuong;
+		public DateTime HanSuDung;
+		public clsHopSua()
+		{
+		}
+		public void DatNhanHieu(string sNhanHieu)
+		{
+			NhanHieu = sNhanHieu;
+		}
+		public string LayNhanHieu()
+		{
+			return NhanHieu;
+		}
+		public void DatTrongLuong(double sTrongLuong)
+		{
+			TrongLuong = sTrongLuong;
+		}
+		public double XuatTL()
+		{
+			return TrongLuong;
+		}
+		public void DatNgay(DateTime sHanSuDung)
+		{
+			HanSuDung = sHanSuDung;
+		}
+		public DateTime XuatNgay()
+		{
+			return HanSuDung;
+		}
+		/*
+		public void Nhap()
+		{
+			Console.WriteLine("Nhap trong luong: ");
+			TrongLuong = double.Parse(Console.ReadLine());
+			Console.WriteLine("Nhap han su dung: ");
+			HanSuDung = DateTime.Parse(Console.ReadLine());
+		}
+		*/
+	}
+}
